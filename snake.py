aae34f6a6f8ea187004d2399f12a420dc6532a4e
Ross <rossco.king78@outlook.com>
2020-08-17 06:10:18 +0930

    updated

--- snake.py
+++ snake.py
@@ -4,6 +4,66 @@ import pygame
 import tkinter as tk
 from tkinter import messagebox
 
+class cube(object):
+    rows = 0
+    w = 0
+    def__init__(self, start, dirnx=1, dirny=0, color=('red')):
+        pass
+
+    def move(self, dirnx, dirny):
+        pass
+
+    def draw(self, surface, eyes=False):
+        pass
+
+class snake(object):
+    body = []
+    turns = {}
+    def __init__(self, color, pos):
+        self.color = color
+        self.head =cube(pos)
+        self.body.append(self.head)
+        self.dirnx = 0
+        self.dirny = 1
+
+    def move(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+            keys = pygame.key.get_pressed()
+
+            for key in keys:
+                if keys[pygame.K_LEFT]:
+                    self.dirnx = -1
+                    self.dirny = 0
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+
+                elif keys[pygame.K_RIGHT]:
+                    self.dirnx = 1
+                    self.dirny = 0
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+                
+                elif keys[pygame.K_UP]:
+                    self.dirnx = 0
+                    self.dirny = -1
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+                
+                elif keys[pygame.K_DOWN::
+                    self.dirnx = 
+                    self.dirny = 1
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+
+    def reset(self, pos):
+        pass
+    def addCube(self):
+        pass
+    def draw(self, surface):
+        pass
+
+
+
+
 
 def drawGrid(w, rows, surface):
     sizeBtwn = w // rows
